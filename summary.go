e882d8e9c8b950f57f99a1fce7775d9fc8254b1d
Shahid Shah <shahid@shah.org>
2019-05-22 19:02:01 -0400

    Expose fields

--- summary.go
+++ summary.go
@@ -8,19 +8,19 @@ import (
 )
 
 type SummaryReporter struct {
-	writer         io.Writer
-	prefix         string
+	Writer         io.Writer
+	Prefix         string
 	mu             sync.RWMutex
 	errorsReported uint
 	maxErrors      uint
 }
 
 func (pr *SummaryReporter) StartReportableActivity(ctx context.Context, summary string, expectedItems int) {
-	fmt.Fprintf(pr.writer, "%s%s\n", pr.prefix, summary)
+	fmt.Fprintf(pr.Writer, "%s%s\n", pr.Prefix, summary)
 }
 
 func (pr *SummaryReporter) StartReportableReaderActivityInBytes(ctx context.Context, summary string, exepectedBytes int64, inputReader io.Reader) io.Reader {
-	fmt.Fprintf(pr.writer, "%s%s\n", pr.prefix, summary)
+	fmt.Fprintf(pr.Writer, "%s%s\n", pr.Prefix, summary)
 	return inputReader
 }
 
@@ -28,14 +28,14 @@ func (pr *SummaryReporter) IncrementReportableActivityProgress(ctx context.Conte
 }
 
 func (pr *SummaryReporter) CompleteReportableActivityProgress(ctx context.Context, summary string) {
-	fmt.Fprintf(pr.writer, "%s%s\n", pr.prefix, summary)
+	fmt.Fprintf(pr.Writer, "%s%s\n", pr.Prefix, summary)
 }
 
 func (pr *SummaryReporter) CollectError(ctx context.Context, err error) bool {
 	pr.mu.Lock()
 	pr.errorsReported++
 	pr.mu.Unlock()
-	fmt.Fprintf(pr.writer, "%s%v\n", pr.prefix, err.Error())
+	fmt.Fprintf(pr.Writer, "%s%v\n", pr.Prefix, err.Error())
 	return !pr.MaxErrorsCollected(ctx)
 }
 
@@ -46,6 +46,6 @@ func (pr *SummaryReporter) MaxErrorsCollected(context.Context) bool {
 }
 
 func (pr *SummaryReporter) CollectWarning(ctx context.Context, code, message string) bool {
-	fmt.Fprintf(pr.writer, "%s%s %s\n", pr.prefix, code, message)
+	fmt.Fprintf(pr.Writer, "%s%s %s\n", pr.Prefix, code, message)
 	return true
 }

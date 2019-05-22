b06c558f2134755ae757cb8343c93766b1e46d87
Shahid Shah <shahid@shah.org>
2019-05-22 19:17:57 -0400

    Expose fields and constructor

--- bar.go
+++ bar.go
@@ -10,16 +10,23 @@ import (
 
 // BarReporter shows a horizontal progress bar on StdOut which is incremented as activities are completed
 type BarReporter struct {
-	prefix    string
-	mu        sync.RWMutex
-	bar       *pb.ProgressBar
-	errList   []error
-	warnList  []struct{ code, message string }
-	maxErrors int
+	Prefix    string
+	ErrList   []error
+	WarnList  []struct{ code, message string }
+	MaxErrors int
+
+	mu  sync.RWMutex
+	bar *pb.ProgressBar
+}
+
+func NewBarReporter(prefix string) *BarReporter {
+	result := new(BarReporter)
+	result.Prefix = prefix
+	return result
 }
 
 func (pr *BarReporter) StartReportableActivity(ctx context.Context, summary string, expectedItems int) {
-	fmt.Printf("%s%s\n", pr.prefix, summary)
+	fmt.Printf("%s%s\n", pr.Prefix, summary)
 	pr.bar = pb.StartNew(expectedItems)
 	pr.bar.ShowCounters = true
 }
@@ -39,24 +46,24 @@ func (pr *BarReporter) IncrementReportableActivityProgressBy(ctx context.Context
 }
 
 func (pr *BarReporter) CompleteReportableActivityProgress(ctx context.Context, summary string) {
-	pr.bar.FinishPrint(fmt.Sprintf("%s%s\n", pr.prefix, summary))
+	pr.bar.FinishPrint(fmt.Sprintf("%s%s\n", pr.Prefix, summary))
 
-	if len(pr.errList) > 0 {
-		for _, err := range pr.errList {
-			fmt.Printf("%s%v\n", pr.prefix, err.Error())
+	if len(pr.ErrList) > 0 {
+		for _, err := range pr.ErrList {
+			fmt.Printf("%s%v\n", pr.Prefix, err.Error())
 		}
 	}
 
-	if len(pr.warnList) > 0 {
-		for _, warning := range pr.warnList {
-			fmt.Printf("%s%s %s\n", pr.prefix, warning.code, warning.message)
+	if len(pr.WarnList) > 0 {
+		for _, warning := range pr.WarnList {
+			fmt.Printf("%s%s %s\n", pr.Prefix, warning.code, warning.message)
 		}
 	}
 }
 
 func (pr *BarReporter) CollectError(ctx context.Context, err error) bool {
 	pr.mu.Lock()
-	pr.errList = append(pr.errList, err)
+	pr.ErrList = append(pr.ErrList, err)
 	pr.mu.Unlock()
 	return !pr.MaxErrorsCollected(ctx)
 }
@@ -64,12 +71,12 @@ func (pr *BarReporter) CollectError(ctx context.Context, err error) bool {
 func (pr *BarReporter) MaxErrorsCollected(context.Context) bool {
 	pr.mu.RLock()
 	defer pr.mu.RUnlock()
-	return pr.maxErrors > 0 && len(pr.errList) > pr.maxErrors
+	return pr.MaxErrors > 0 && len(pr.ErrList) > pr.MaxErrors
 }
 
 func (pr *BarReporter) CollectWarning(ctx context.Context, code, message string) bool {
 	pr.mu.Lock()
-	pr.warnList = append(pr.warnList, struct{ code, message string }{code: code, message: message})
+	pr.WarnList = append(pr.WarnList, struct{ code, message string }{code: code, message: message})
 	pr.mu.Unlock()
 	return true
 }
